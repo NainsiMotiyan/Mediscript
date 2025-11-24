7241e0707e6e1b1489b28214611f28bba7333d8c
Nainsi Motiyan <nainsi759@gmail.com>
2025-11-24 23:17:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://mediscript-frontend.onrender.com",
+  credentials: true
+}));
 
 // api endpoints
 app.use('/api/doctor',doctorRouter)
